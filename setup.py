0352c6defdf3e6c02a0b11a99313afa6f152073c
Arun Ahuja <aahuja11@gmail.com>
2016-01-15 10:30:34 -0500

    bumping version for rna and proper download handling

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except:
 if __name__ == '__main__':
     setup(
         name='pytcga',
-        version="0.0.1",
+        version="0.0.2",
         description="Store and query public TCGA data",
         author="Arun Ahuja",
         author_email="aahuja11 {at} gmail {dot}",
