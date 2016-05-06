f8ef402133acdfa1a6b86d822f2e689250a72770
B. Arman Aksoy <arman@aksoy.org>
2016-05-06 16:57:12 -0400

    add support for loading all known studies and abbr.s

--- pytcga/tcga_utils.py
+++ pytcga/tcga_utils.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import requests
+
+# http://stackoverflow.com/questions/22604564/how-to-create-a-pandas-dataframe-from-string
+import sys
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+from .urls import CODE_TABLE_ADDRESS
 
 def load_tcga_tabfile(path,
                       skiprows=0):
@@ -7,10 +17,22 @@ def load_tcga_tabfile(path,
                           skiprows=skiprows,
                           nrows=10).columns
     df = pd.read_csv(path,
-                    sep='\t', 
+                    sep='\t',
                     skiprows=skiprows + 1,
                     header=0,
                     names=columns,
                     na_values='[Not Available]')
 
     return df
+
+def load_studies():
+    payload = {'exportType': 'csv',
+               'dir': 'undefined',
+               'sort': 'undefined',
+               'codeTablesReport': 'bcrBatchCode'}
+    r = requests.post(CODE_TABLE_ADDRESS, payload)
+    df = pd.DataFrame.from_csv(StringIO(r.text))
+    return df[['Study Abbreviation', 'Study Name']] \
+            .drop_duplicates() \
+            .sort_values(by="Study Abbreviation") \
+            .reset_index(drop=True)
--- pytcga/urls.py
+++ pytcga/urls.py
@@ -7,3 +7,5 @@ TCGA_STATUS_CHECK_URL_FIELD = 'status-check-url'
 
 REQUEST_ADDRESS = TCGA_NIH_REQUEST_ADDRESS
 JOB_STATUS_ADDRESS = "http://tcga-data.nci.nih.gov/tcga/damws/jobprocess/json/ticket/"
+
+CODE_TABLE_ADDRESS = "https://tcga-data.nci.nih.gov/datareports/codeTablesExport.htm"
